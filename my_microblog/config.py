421ee9f03b70f66d8aaac343b1f439c083886985
PinkWitch13 <pinkwitch13@proton.me>
2025-04-18 15:53:19 +0200

    posts showing in my_microblog with next/previous page function, and posts per page limitation

--- my_microblog/config.py
+++ my_microblog/config.py
@@ -12,6 +12,6 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['my_email@example.com']
-    POSTS_PER_PAGE = 3
+    POSTS_PER_PAGE = 25
 
     
